ee4ba533e93e61931b49061351ca16b39397db94
Nur Islam <nurislam0333@gmail.com>
2019-06-13 17:36:49 +0600

    server|database

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
